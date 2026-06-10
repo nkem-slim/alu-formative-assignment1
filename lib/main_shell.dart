2c412cdfd8faa2ecb455693ca8b4a2d53df9eb79
bienvenue cedrick <cedrick4smith@gmail.com>
2026-06-10 23:52:18 +0200

    Update MainShell to accept and pass AuthService to profile screen

--- lib/main_shell.dart
+++ lib/main_shell.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_colors.dart';
+import 'features/auth/data/auth_service.dart';
 import 'features/home/presentation/screens/home_screen.dart';
 import 'features/events/presentation/screens/events_screen.dart';
 import 'features/my_events/presentation/screens/my_events_screen.dart';
 import 'features/profile/presentation/screens/profile_screen.dart';
 
 class MainShell extends StatefulWidget {
-  const MainShell({super.key});
+  final AuthService authService;
+  const MainShell({super.key, required this.authService});
 
   @override
   State<MainShell> createState() => _MainShellState();
@@ -14,13 +16,18 @@ class MainShell extends StatefulWidget {
 
 class _MainShellState extends State<MainShell> {
   int _currentIndex = 0;
+  late final List<Widget> _screens;
 
-  final List<Widget> _screens = const [
-    HomeScreen(),
-    EventsScreen(),
-    MyEventsScreen(),
-    ProfileScreen(),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      const HomeScreen(),
+      const EventsScreen(),
+      const MyEventsScreen(),
+      ProfileScreen(authService: widget.authService),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
